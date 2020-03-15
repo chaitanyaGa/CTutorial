5df5c18d6768a9a7fcc7061c3057a98db9785113
chaitanyaGa <chaitanya.gaikwad93@gmail.com>
2020-03-15 07:24:36 +0530

    Printf function

--- DP_42.cpp
+++ DP_42.cpp
@@ -1,1 +1,12 @@
+#include<stdio.h>
 
+using namespace std;
+//we cant do computation inside printf function.
+int main()
+{
+  int i = 6, j = 8;
+  printf("%d",5+7);
+  printf("%d",i+j);
+  
+  return 0;
+}
